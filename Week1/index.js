fda02ab8b2624d6f1fd70ea1adc5af16a171a949
peterr-ODI <peter.rawsthorne@oceanset.com>
2021-12-30 11:37:16 -0330

    Explore serving up data as http server

--- Week1/index.js
+++ Week1/index.js
@@ -1,13 +1,54 @@
 var http = require('http');
 var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('test.html', function(err, data) {
-    if(err)
-        console.log(err);
-    else {
+
+//serveText('Hello World');
+//serveHtml('<html><body>Woot Woot</body></html>');
+//serveFile('test.html');
+serveJson('user.json')
+
+function serveText(theText){
+  http.createServer(function (req, res) {
+    console.log('test was served.')
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(theText);
+    res.end();
+  }).listen(3001);
+}
+
+function serveHtml(theHtml){
+  http.createServer(function (req, res) {
+    console.log('html was served.')
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-    }   
-  });
-}).listen(8080);
+    res.write(theHtml);
+    res.end();
+  }).listen(3002);
+}
+function serveFile(filename) {
+  http.createServer(function (req, res) {
+    fs.readFile(filename, function(err, data) {
+      if(err)
+          console.log(err);
+      else {
+        console.log('file ' + filename + ' was served.')
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      }   
+    });
+  }).listen(3003);
+}
+
+function serveJson(jsonfile) {
+  http.createServer(function (req, res) {
+    fs.readFile(jsonfile, function(err, data) {
+      if(err)
+          console.log(err);
+      else {
+        console.log('json file ' + jsonfile + ' was served.')
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.write(data);
+        return res.end();
+      }   
+    });
+  }).listen(3004);
+}
