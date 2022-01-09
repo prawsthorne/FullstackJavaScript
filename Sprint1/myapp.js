bc05b3663f5ace0af40ce25aff85a13688272f22
peterr-ODI <peter.rawsthorne@oceanset.com>
2022-01-09 13:40:17 -0330

    basic CLI command structure

--- Sprint1/myapp.js
+++ Sprint1/myapp.js
@@ -1,5 +1,44 @@
 /*************************
- * init - creates the folder structure 
- * config - displays the config .json file
- * token - generates a token for a given username
+ * File Name: myapp.js
+ * Purpose: The main routines to start the initialization app
+ * 
+ * Commands:
+myapp init              creates the folder structure, creates config files
+myapp config            displays the list of different config .json files
+myapp config <file>     displays a specific config files contents
+myapp token             displays a count and list of tokens
+myapp token <username>  generates a token for a given username, saves tokens to json file
+ *
+ * Created Date: 09 Jan 2022
+ * Authors:
+ * PJR - Peter Rawsthorne
+ * Revisions:
+ * Date, Author, Description
+ * 09 Jan 2022, PJR, File created
+ *
  *************************/
+const fs = require("fs");
+const myArgs = process.argv.slice(2);
+if(myArgs.length > 1) console.log('the args: ', myArgs);
+
+switch (myArgs[0]) {
+    case 'init':
+    case 'i':
+        console.log(myArgs[0], ' - initialize the app.');
+        break;
+    case 'config':
+    case 'c':
+        console.log(myArgs[0], ' - display the configuration file');
+        break;
+    case 'token':
+    case 't':
+        console.log(myArgs[0], ' - generate a user token');
+        break;  
+    case 'help':
+    case 'h':
+    default:
+        fs.readFile(__dirname + "/usage.txt", (error, data) => {
+            if(error) throw error;
+            console.log(data.toString());
+        });
+}
