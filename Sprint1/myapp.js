555b70f3305a6fda12fd7220c8aaa7561bc72619
peterr-ODI <peter.rawsthorne@oceanset.com>
2022-01-09 07:50:56 -0330

    Working on the sprint

---
+++ Sprint1/myapp.js
@@ -0,0 +1,5 @@
+/*************************
+ * init - creates the folder structure 
+ * config - displays the config .json file
+ * token - generates a token for a given username
+ *************************/
