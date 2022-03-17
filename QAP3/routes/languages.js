cd329b777b806cd39e89b8e0d32613eb75b4fa66
peter rawsthorne <peter@rawsthorne.org>
2022-03-17 14:06:40 -0230

    PUT, PATCH, DELETE... only for http

--- QAP3/routes/languages.js
+++ QAP3/routes/languages.js
@@ -30,17 +30,24 @@ router.post('/', async (req, res) => {
     res.redirect('/languages/');
 });
 
-router.patch('/', async (req, res) => {
+// PUT, PATCH, and DELETE are part of HTTP, not a part of HTML
+// Therefore, <form method="PUT" ...> doesn't work, but it does work for RESTful API
+router.put('/:id', async (req, res) => {
+    console.log('put: ' + req.params.id);
+ //   await languagesDal.putLanguage(req.params.id);
+    res.redirect('/languages/');
+});
+
+router.patch('/:id', async (req, res) => {
     console.log('patch: ' + req.params.id);
  //   await languagesDal.patchLanguage(req.params.id);
     res.redirect('/languages/');
 });
 
-router.delete('/', async (req, res) => {
+router.delete('/:id', async (req, res) => {
     console.log('delete: ' + req.params.id);
  //   await languagesDal.deleteLanguage(req.params.id);
     res.redirect('/languages/');
 });
-
 
 module.exports = router
